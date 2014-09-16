7bbbbb82a84e529d33a49076bb7f7c27cda997a2
giorgioscappaticcio <ooohh1@hotmail.it>
2014-09-17 00:14:46 +0100

    update

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the stopemotionApp
  */
 angular.module('stopemotionApp')
-  .controller('MainCtrl', function ($scope, $document, $window) {
+  .controller('MainCtrl', function ($scope, $document, $window, $rootScope) {
     	var winHeight = $window.innerHeight;
 
     	$scope.winHeight = {
     		'height' : winHeight+20 + 'px'
     	}
+
+    	$rootScope.windowWidth = $window.outerWidth;
+ 		angular.element($window).bind('resize',function(){
+  			$rootScope.windowWidth = $window.outerWidth;
+  			$rootScope.$apply('windowWidth');
+ 		});
+
+ 		$rootScope.$watch('windowWidth',function(newVal, oldVal){
+  			var winHeight = $window.innerHeight;
+
+	    	$scope.winHeight = {
+	    		'height' : winHeight+20 + 'px'
+	    	};
+ 		})
   });
